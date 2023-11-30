5a77ca13492bf9a802faa652f7cd470cd58b38b1
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-11-30 12:23:27 +0100

    Create customers chart

--- ds02 - data analyst/ex01/chart.py
+++ ds02 - data analyst/ex01/chart.py
@@ -1,5 +1,5 @@
-import re
 import psycopg2
+from datetime import datetime
 from abc import ABC
 from abc import abstractmethod
 import matplotlib.pyplot as plt
@@ -25,7 +25,7 @@ class Customers(Connection):
     def __init__(self, username: str, password: str, database: str) -> None:
         super().__init__(username, password, database)
 
-    def pie(self, table: str):
+    def customers(self):
         self.cursor.execute(f"""
             SELECT
                 DATE(event_time) AS date,
@@ -34,6 +34,7 @@ class Customers(Connection):
                 customers
             WHERE
                 event_type = 'purchase'
+                AND event_time <= '2023-01-31'
             GROUP BY 
                 DATE(event_time)
             ORDER BY 
@@ -42,23 +43,55 @@ class Customers(Connection):
 
         dataset = dict(self.cursor.fetchall())
 
-        dates = list(dataset.keys())
+        dates = [datetime.strptime(str(date), '%Y-%m-%d') for date in dataset.keys()]
         values = list(dataset.values())
 
-        plt.plot(dates, values, linestyle='-')
+        ax = plt.axes()
+        ax.set_facecolor("#eaeaf3")
+        ax.spines['top'].set_edgecolor('white')
+        ax.spines['right'].set_edgecolor('white')
+        ax.spines['bottom'].set_edgecolor('white')
+        ax.spines['left'].set_edgecolor('white')
+        ax.tick_params(axis='both', color='white')
+
+        plt.plot(dates, values, linewidth=1, linestyle='-')
         plt.xlabel("Years")
         plt.ylabel("Number of customers")
-        tick_positions = [0, len(dates) // 4, 2 * len(dates) // 4, 3 * len(dates) // 4]
-        tick_labels = ["Oct", "Nov", "Dec", "Jan"]
-        plt.xticks(tick_positions, tick_labels)
+
+        months_ticks = [date for date in dates if date.day == 1]
+        months_labels = [date.strftime('%b') for date in months_ticks if date.day == 1]
+        plt.xticks(months_ticks, months_labels)
+
         plt.xlim(dates[0], dates[-1])
+        plt.grid(color='white')
         plt.show()
+
+    def sales(self):
+        self.cursor.execute(f"""
+            SELECT
+                DATE_TRUNC('month', event_time) AS month,
+                COUNT(*) AS count_per_date
+            FROM 
+                customers
+            WHERE
+                event_type = 'purchase'
+                AND event_time <= '2023-01-31'
+            GROUP BY 
+                DATE_TRUNC('month', event_time)
+            ORDER BY 
+                DATE_TRUNC('month', event_time);
+        """)
+
+        dataset = dict(self.cursor.fetchall())
+
+        print(dataset)
 
 
 def main():
     try:
         customer = Customers('piscineds', 'guysharony', 'mysecretpassword')
-        customer.pie('customers')
+        customer.customers()
+        customer.sales()
         customer.close()
     except Exception as err:
         print(f'Error: {err}')

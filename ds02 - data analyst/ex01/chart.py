acc83dac1ea5c25c379de9f1b232f70839a7d062
Guy Sharony <40563195+guysharony@users.noreply.github.com>
2023-12-01 10:19:57 +0100

    Graphic fixed

--- ds02 - data analyst/ex01/chart.py
+++ ds02 - data analyst/ex01/chart.py
@@ -1,3 +1,4 @@
+import math
 import psycopg2
 from datetime import datetime
 from abc import ABC
@@ -46,13 +47,7 @@ class Customers(Connection):
         dates = [datetime.strptime(str(date), '%Y-%m-%d') for date in dataset.keys()]
         values = list(dataset.values())
 
-        ax = plt.axes()
-        ax.set_facecolor("#eaeaf3")
-        ax.spines['top'].set_edgecolor('white')
-        ax.spines['right'].set_edgecolor('white')
-        ax.spines['bottom'].set_edgecolor('white')
-        ax.spines['left'].set_edgecolor('white')
-        ax.tick_params(axis='both', color='white')
+        plt.figure()
 
         plt.plot(dates, values, linewidth=1, linestyle='-')
         plt.xlabel("Years")
@@ -69,29 +64,83 @@ class Customers(Connection):
     def sales(self):
         self.cursor.execute(f"""
             SELECT
-                DATE_TRUNC('month', event_time) AS month,
-                COUNT(*) AS count_per_date
+                TO_CHAR(event_time, 'YYYY-MM') AS month,
+                CAST((SUM(price) / 1000000.0 * 0.8) AS DECIMAL(10, 2)) AS price
             FROM 
                 customers
             WHERE
                 event_type = 'purchase'
                 AND event_time <= '2023-01-31'
             GROUP BY 
-                DATE_TRUNC('month', event_time)
+                TO_CHAR(event_time, 'YYYY-MM')
             ORDER BY 
-                DATE_TRUNC('month', event_time);
+                TO_CHAR(event_time, 'YYYY-MM');
         """)
 
         dataset = dict(self.cursor.fetchall())
 
-        print(dataset)
+        months = [datetime.strptime(str(date), '%Y-%m').strftime('%b') for date in dataset.keys()]
+        counts = [sale for sale in dataset.values()]
 
+        plt.bar(months, counts)
+        plt.ylabel('total sales in million of ₳')
+        plt.xlabel('month')
+        plt.tick_params(axis='both', color='white')
+
+        plt.show()
+
+    def spend_by_customer(self):
+        self.cursor.execute(f"""
+            SELECT
+                transaction_date,
+                AVG(daily_total) AS average_spent_of_users
+            FROM (
+                SELECT
+                    user_id,
+                    DATE(event_time) AS transaction_date,
+                    SUM(price) AS daily_total
+                FROM
+                    customers
+                WHERE
+                    event_type = 'purchase'
+                    AND event_time <= '2023-01-31'
+                GROUP BY
+                    user_id,
+                    DATE(event_time)
+            ) AS user_daily_totals
+            GROUP BY
+                transaction_date
+            ORDER BY
+                transaction_date;
+        """)
+
+        dataset = dict(self.cursor.fetchall())
+
+        dates = [datetime.strptime(str(date), '%Y-%m-%d') for date in dataset.keys()]
+        values = list(dataset.values())
+
+        plt.figure()
+
+        plt.plot(dates, values, color='blue', linewidth=1)
+        plt.fill_between(dates, values, color='blue')
+        plt.ylabel("average spend/customers in ₳")
+
+        months_ticks = [date for date in dates if date.day == 1]
+        months_labels = [date.strftime('%b') for date in months_ticks if date.day == 1]
+        plt.xticks(months_ticks, months_labels)
+        plt.yticks(range(0, math.ceil(max(values) / 5) * 5 + 1, 5))
+
+        plt.xlim(dates[0], dates[-1])
+        plt.ylim(bottom=0)
+        plt.grid(which='both', linestyle='-', linewidth='0')
+        plt.show()
 
 def main():
     try:
         customer = Customers('piscineds', 'guysharony', 'mysecretpassword')
         customer.customers()
         customer.sales()
+        customer.spend_by_customer()
         customer.close()
     except Exception as err:
         print(f'Error: {err}')
